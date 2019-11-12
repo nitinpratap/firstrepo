091c1fa2b3498fc4a2a784868f033f986341c838
nitinp <nitinp@Pradeep-Bisht.tv18.com>
2019-11-12 18:12:15 +0530

    changes saved

--- test.php
+++ test.php
@@ -1,10 +1,4 @@
 <?php
 echo "test 1";
-echo "saved with permanent";
-echo "Working with temp changes";
-echo "Working with temp changes2";
-echo "Working with temp changes3";
-echo "Working with temp changes4";
-echo "Working with temp changes5";
-echo "Working with temp changes6";
+echo "test 2";
 ?>
