4e3c0db8358be0855782c0463f5203a55cd326f6
nitinpratap <nitin_pratapmca2007@yahoo.com>
2021-10-07 03:29:45 +0530

    add test.php to master

---
+++ test.php
@@ -0,0 +1,12 @@
+?php
+echo "test 1.....";
+echo "saved with permanent";
+echo "Working with temp changes";
+echo "Working with temp changes2";
+echo "Working with temp changes3";
+echo "Working with temp changes4";
+echo "Working with temp changes5";
+echo "Working with temp changes by netbeans";
+echo "Working with temp changes by vscode";
+
+?>
